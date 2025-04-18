493eb9b0c7f5294eedb17eeedd75e942651c75cc
redwanpp <smredwan.info@gmail.com>
2025-04-18 16:40:36 +0600

    completed login, logout, updateProfilePic, checkAuth api

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,21 @@
 import express from "express";
 
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  checkAuth,
+  updateProfilePic,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.put("update-profile-pic", protectRoute, updateProfilePic);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
